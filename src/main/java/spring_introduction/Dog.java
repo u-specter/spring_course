773c6161f597d102dbfcb38e646c24868104e2a8
Umidjon <umidjon.abduxamidov@fido.uz>
2023-08-24 11:18:36 +0500

    add new applicationContext3 for sutdying annotations

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,8 +1,26 @@
 package spring_introduction;
 
 public class Dog implements Pet{
+//    private String name;
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
     public Dog() {
-        System.out.println("Dog bean si created");
+        System.out.println("Dog bean is created");
+    }
+
+    public void init(){
+        System.out.println("Class dog: init method");
+    }
+
+    public void destroy(){
+        System.out.println("Class dog: destroy method");
     }
 
     @Override
--- src/main/java/spring_introduction/Test4.java
+++ src/main/java/spring_introduction/Test4.java
@@ -1,5 +1,18 @@
 package spring_introduction;
 
-public class Test4 {
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext2.xml");
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        myDog.setName("Belka");
+//        Dog youDog = context.getBean("myPet", Dog.class);
+//        youDog.setName("Strelka");
+//        System.out.println("Something" + (myDog==youDog));
+//        System.out.println(myDog);
+//        System.out.println(youDog);
+        context.close();
+    }
 }
